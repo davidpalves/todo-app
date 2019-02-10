03bd8fbb63507c9c787fb75b0a4f6c12d068d3da
david <davidpierrealves21@gmail.com>
2019-02-10 02:42:07 -0300

    Todo App Backend

---
+++ backend/todos/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class ListTodo(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
