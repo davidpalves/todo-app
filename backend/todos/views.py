f93e06f741f71a7fb30b9a5b631ae2d2d8ee5b25
david <davidpierrealves21@gmail.com>
2019-02-19 16:17:48 -0300

    Users can now access just their own todos or the ones the contribute to

--- backend/todos/views.py
+++ backend/todos/views.py
@@ -1,12 +1,20 @@
 from rest_framework import viewsets
 from .models import Todo, Task
 from .serializers import TodoSerializer, TaskSerializer
+from django.db.models import Q
+
 
 
 class TodoViewSet(viewsets.ModelViewSet):
-    queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-    
+
+    def get_queryset(self):
+        user = self.request.user
+
+        todos = Todo.objects.filter(
+            Q(contributors__id=user.id) | Q(owner_id=user.id))
+
+        return todos    
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
